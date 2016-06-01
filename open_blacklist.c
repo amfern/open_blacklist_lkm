bf2c329198d533af33d29c7f8e2d5579f9ca4e5c
Ilia Guterman <ilia@cellrox.com>
2016-06-01 09:17:01 -0400

    print ino on open file

--- open_blacklist.c
+++ open_blacklist.c
@@ -5,6 +5,9 @@
 #include <asm/cacheflush.h>
 #include <linux/syscalls.h>
 #include <linux/delay.h>    // loops_per_jiffy
+#include <linux/fs.h>
+#include <linux/slab.h>     // kmalloc
+#include <linux/kallsyms.h>
 
 #define CR0_WP 0x00010000   // Write Protect Bit (CR0:16)
 
@@ -35,6 +38,13 @@ static inline int make_ro(unsigned long address)
 	return 0;
 }
 
+/* static inline int same_file(int fd1, int fd2) { */
+/*     struct stat stat1, stat2; */
+/*     if(fstat(fd1, &stat1) < 0) return -1; */
+/*     if(fstat(fd2, &stat2) < 0) return -1; */
+/*     return (stat1.st_dev == stat2.st_dev) && (stat1.st_ino == stat2.st_ino); */
+/* } */
+
 /**
  * Addresses in my System.map file
  * ffffffff81175dc0 T sys_close
@@ -63,11 +73,29 @@ unsigned long **find_sys_call_table() {
 
 long my_sys_open(const char __user *filename, int flags, int mode) {
     long ret;
+	struct kstat *stat;
+	stat = kmalloc(sizeof (struct kstat), GFP_KERNEL);
 
-    ret = orig_sys_open(filename, flags, mode);
-    printk(KERN_DEBUG "file %s has been opened with mode %d\n", filename, mode);
-    
-    return ret;
+	vfs_stat(filename, stat);
+
+	if (!stat) {
+		/* the allocation failed - handle appropriately */
+		printk(KERN_DEBUG "failed allocate memory QQ");
+	}
+	
+    // if(fstat(fd1, &stat) < 0) return -1;
+	// nameidata *nd;
+
+	// this will also cache the inode for orig_sys_open
+	// open_namei(filename, flags, mode, nd);
+
+	// ret is a filedescriptor
+	ret = orig_sys_open(filename, flags, mode);
+    printk(KERN_DEBUG "file %s has been opened with mode %d\n, ino %llu", filename, mode, stat->ino);
+
+	kfree(stat);
+
+	return ret;
 }
 
 static int __init syscall_init(void)
